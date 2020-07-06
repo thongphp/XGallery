7eb073eb42d96a33d22455e0a01a5794fffd6244
Viet Vu <jooservices@gmail.com>
2020-07-06 14:13:09 +0700

    ^ Batdongsan tests

---
+++ app/Crawlers/Tests/Unit/Crawlers/BatdongsanTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Crawlers\Tests\Unit\Crawlers;
+
+use App\Crawlers\Crawler\Batdongsan;
+use App\Crawlers\Tests\TestCase;
+use App\Models\BatdongsanModel;
+use Illuminate\Contracts\Foundation\Application;
+
+class BatdongsanTest extends TestCase
+{
+    /**
+     * @var Batdongsan|Application|mixed
+     */
+    private Batdongsan $crawler;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->crawler = app(Batdongsan::class);
+    }
+
+    public function testGetItem()
+    {
+        $items = $this->crawler->getItem('https://batdongsan.com.vn/ban-can-ho-chung-cu-duong-ven-bien-xa-phuoc-thuan-prj-ho-tram-complex/chi-1-4-ty-quy-khach-hang-so-huu-ngay-comlex-so-ng-so-huu-vinh-vien-pr26133982');
+        $this->assertInstanceOf(BatdongsanModel::class, $items);
+    }
+}
