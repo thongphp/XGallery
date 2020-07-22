a631c8856be8a14d80f7546f6027629c2b68326a
Viet Vu <jooservices@gmail.com>
2020-07-22 20:49:37 +0700

    # Update ci.yml

--- app/Crawlers/Tests/Unit/Crawlers/BatdongsanTest.php
+++ app/Crawlers/Tests/Unit/Crawlers/BatdongsanTest.php
@@ -26,19 +26,19 @@ class BatdongsanTest extends TestCase
         return [
             [
                 'url' => 'https://batdongsan.com.vn/ban-can-ho-chung-cu-duong-ven-bien-xa-phuoc-thuan-prj-ho-tram-complex/chi-1-4-ty-quy-khach-hang-so-huu-ngay-comlex-so-ng-so-huu-vinh-vien-pr26133982',
-                'expectTitle' => 'CHỈ 1.4 TỶ QÚY KHÁCH HÀNG SỞ HỮU NGAY CĂN HỘ BIỂN HỒ TRÀM COMLEX, SỔ HỒNG SỞ HỮU VĨNH VIỄN',
+                'expectTitle' => 'Chỉ 1.4 tỷ qúy khách hàng sở hữu ngay căn hộ biển Hồ Tràm Comlex, sổ hồng sở hữu vĩnh viễn',
             ],
             [
                 'url' => 'https://batdongsan.com.vn/ban-nha-biet-thu-lien-ke-duong-tan-mai-phuong-hoang-van-thu-4-prj-louis-city-hoang-mai/co-hoi-vang-mua-suat-ngoai-giao-gia-tot-dot-dau-vao-ten-truc-tiep-cdt-du-an-mai-pr24905461',
-                'expectTitle' => 'CƠ HỘI ĐẦU TƯ SUẤT NGOẠI GIAO LOUIS HOÀNG MAI - VÀO TÊN TRỰC TIẾP CĐT - GIÁ ƯU ĐÃI'
+                'expectTitle' => 'Bán suất ngoại giao Louis City Hoàng Mai - Vào tên trực tiếp CĐT - giá ưu đãi'
             ],
             [
                 'url' => 'https://batdongsan.com.vn/ban-dat-duong-bau-sen-phuong-bau-sen/chinh-chu-lo-25-303-mt-kinh-doanh-ngay-ubnd-p-sen-pr26162851',
-                'expectTitle' => 'CHÍNH CHỦ LÔ 25/303 MT KINH DOANH NGAY UBND P. BÀU SEN'
+                'expectTitle' => 'Chính chủ lô 25/303 mt kinh doanh ngay UBND P. Bàu Sen'
             ],
             [
                 'url' => 'https://batdongsan.com.vn/ban-nha-rieng-duong-70-2-xa-van-canh-1/hot-co-hoi-co-o-to-do-cua-tai-phuong-ha-noi-pr26120640',
-                'expectTitle' => 'HOT CƠ HỘI CÓ NHÀ Ô TÔ ĐỖ CỬA TẠI VÂN CANH, PHƯƠNG CANH, HÀ NỘI'
+                'expectTitle' => 'Hot cơ hội có nhà ô tô đỗ cửa tại Vân Canh, Phương Canh, Hà Nội'
             ]
         ];
     }
@@ -66,6 +66,6 @@ class BatdongsanTest extends TestCase
 
     public function testGetPagesCount(): void
     {
-        $this->assertGreaterThan(9824, $this->crawler->getIndexPagesCount('https://batdongsan.com.vn/nha-dat-ban'));
+        $this->assertGreaterThan(9000, $this->crawler->getIndexPagesCount('https://batdongsan.com.vn/nha-dat-ban'));
     }
 }
